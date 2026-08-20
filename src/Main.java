d54de8ae79756a590313ec049018ecb21563c2bc
ridham sharma <ridhamsharma6890@gmail.com>
2026-08-21 01:49:57 +0530

    Added Scanner input program.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-        int a = 30;
-        int b = 25;
-        int multiply = a * b;
-        System.out.println(multiply);
 
-
+Scanner sc = new Scanner(System.in);
+String name = sc.next();
+System.out.println(name);
     }
 
 
