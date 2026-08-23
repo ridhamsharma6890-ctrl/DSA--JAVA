0c9be6086334fd68768422d97c55067752d7efc1
ridham sharma <ridhamsharma6890@gmail.com>
2026-08-23 16:10:04 +0530

    Added Scanner input program.

---
+++ src/Scannerpractice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Scannerpractice{
+    public static void Main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name= sc.next();
+        System.out.println(name);
+
+    }
+
+
+
+
+
+}
