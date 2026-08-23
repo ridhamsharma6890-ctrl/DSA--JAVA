83ba797b23320c9abe99a801460191afa0c67559
ridham sharma <ridhamsharma6890@gmail.com>
2026-08-24 00:29:43 +0530

    Added switch statement program.

---
+++ src/switchstatement.java
@@ -0,0 +1,28 @@
+public class switchstatement {
+    public static void main(String[] args) {
+        int day = 9
+                ;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
+
+
+
+
+
+
+
+
