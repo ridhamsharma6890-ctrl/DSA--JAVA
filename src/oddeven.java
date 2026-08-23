b10ad9ffacc8ed69119ca9a8af681016c112d5a3
ridham sharma <ridhamsharma6890@gmail.com>
2026-08-23 16:32:28 +0530

    Added odd/even program.

--- src/oddeven.java
+++ src/oddeven.java
@@ -1,24 +1,30 @@
+import java.net.StandardSocketOptions;
 import java.util.*;
+public class oddeven {
 
-public class Conditions {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         int x = sc.nextInt();
-
         if(x % 2 == 0) {
-
             System.out.println("Even");
+
 
         } else{
             System.out.println("odd");
         }
 
 
+
+
+
     }
 
 
+
+
+
+
+
+
 }
-
-
-
-
