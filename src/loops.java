e402b798529637e62bda96eab4d047a21f38e257
ridham sharma <ridhamsharma6890@gmail.com>
2026-08-24 23:28:53 +0530

    Added while Loop program.

--- src/loops.java
+++ src/loops.java
@@ -1,18 +1,11 @@
 public class loops {
     public static void main(String args []){
-        for(int counter = 0; counter < 10; counter = counter + 1){
-
-            System.out.println("Hello World");
+        int i = 0;
+        while(i < 11){
+            System.out.println(i);
+            i = i + 1;
 
         }
-
-
-
-
     }
-
-
-
-
 
 }
