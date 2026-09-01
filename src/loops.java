7ad0d6fab0880c8d84122bc03521206120933c28
ridham sharma <ridhamsharma6890@gmail.com>
2026-09-01 07:41:07 +0530

    Added for loops program.

--- src/loops.java
+++ src/loops.java
@@ -1,1 +1,11 @@
 
+public class loops {
+    public static void main(String args[]) {
+        for (int counter = 0; counter < 3; counter = counter + 1) {
+            System.out.println("Hello i am Ridham");
+
+
+        }
+
+    }
+}
